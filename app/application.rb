61abd9da4b80095ec032d0c08551f5de1718456a
Warren Harper II <warrenharper2@att.net>
2020-02-10 20:55:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,24 @@
 class Application
 
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/@@item/"
-      resp.write "Item Price"
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+      item = @@items.find{|i| i.name == item_name}
+
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else
+        resp.write item.price
+      end
     else
-      resp.write "Item not found"
+      resp.write "Route not found"
       resp.status = 404
     end
 
