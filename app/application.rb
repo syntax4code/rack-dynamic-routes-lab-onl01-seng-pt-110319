7a59277a8e3af35c27f9309dd576134551aef9fd
Warren Harper II <warrenharper2@att.net>
2020-02-10 20:51:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/Item/"
+    if req.path=="/@@item/"
       resp.write "You requested the songs"
     else
       resp.write "Route not found"
